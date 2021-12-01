dec7292a1404cba4ed94be50437a4908c9ce5a0b
nanaodao <95140282+nanaodao@users.noreply.github.com>
2021-12-01 20:36:56 +0800

    Update food.js

--- data/food.js
+++ data/food.js
@@ -1,6 +1,26 @@
 var _data = [
     {
-        'name':'店铺转让',
-        'url':'http://www.zrang.com'
+        'name':'下厨房',
+        'url':'http://www.xiachufang.com/'
     },
+    {
+        'name':'豆果美食',
+        'url':'http://www.douguo.com/'     
+    },
+    {
+        'name':'美食杰',
+        'url':'http://www.meishij.net/'     
+    },
+    {
+        'name':'美食天下',
+        'url':'http://www.meishichina.com/'     
+    },
+    {
+        'name':'美食吧',
+        'url':'http://www.sbar.com.cn/'     
+    },
+    {
+        'name':'菜肴之家',
+        'url':'http://www.haozuo123.com/'     
+    }
 ];
